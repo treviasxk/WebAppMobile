876988a3ea88c4ae617948ac260382f784989cc4
Trevias Xk <trevias@live.com>
2021-03-21 20:49:46 -0300

    • Adicionado tela de carregamento. • Nova gif salvo na pasta img.

--- data/js/main.js
+++ data/js/main.js
@@ -4,16 +4,21 @@ function OpenNavigation(Open){
         document.body.style.overflow = "hidden";
     }else{
         Navigate.style.left = "-100%";
-        document.body.style.overflow = "visible";
+        document.body.style.overflowY = "visible";
     }
     
     if(Navigate.style.left == "0px"){
         Navigate.style.left = "-100%";
-        document.body.style.overflow = "visible";
+        document.body.style.overflowY = "visible";
     }else{
         Navigate.style.left = "0px";
         document.body.style.overflow = "hidden";
     }
+}
+
+function CloseScreenLoading(){
+    document.body.style.overflowY = "visible";
+    ScreenLoading.style.display = "none";
 }
 
 function BackPage(){
