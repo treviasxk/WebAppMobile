f95a314db3994f8dddada740a1fc45a3c09fd4a0
Trevias Xk <trevias@live.com>
2021-07-15 20:18:33 -0300

    • versão estável

--- data/js/main.js
+++ data/js/main.js
@@ -1,9 +1,8 @@
 var Loading = true;
 
 window.addEventListener('load', () => {
+    Content.style.left = "0px";
     CloseScreenLoading();
-    
-    //Configurações Swipe
     var content = document.getElementById('Content');
     if(document.body.contains(document.getElementById('Navigate'))){
         document.getElementById("OpenMenu").onclick = function() {
@@ -12,7 +11,6 @@ window.addEventListener('load', () => {
         document.getElementById("Content").onclick = function() {
             OpenNavigation(false);
         };
-
 
         swipedetect(content, (swipedir) => {
             if (swipedir =='right')
@@ -27,7 +25,7 @@ window.addEventListener('load', () => {
         document.getElementById("BackPage").onclick = function() {
             BackPage();
         };
-        
+
         swipedetect(content, (swipedir) => {
             if (swipedir =='right')
                 BackPage();
@@ -36,36 +34,34 @@ window.addEventListener('load', () => {
 });
 
 function BackPage(){
-    Content.style.left = "100%";
+    Content.style.left = "200%";
     setTimeout(function() {
         window.history.back();
     }, 200);
 }
 
-
 //Fechar ou abrir menu lateral
 function OpenNavigation(Open){
     if(Open == false){
         Navigate.style.left = "0px";
-        Content.style.opacity = "1";
+        Content.style.opacity = null;
+        document.getElementById("AppBar").style.top = "0px";
     }else{
         Navigate.style.left = "-100%";
         Content.style.opacity = "0.5";
+        document.getElementById("AppBar").style.top = "-55px";
     }
     
     if(Navigate.style.left == "0px"){
         Navigate.style.left = "-100%";
         if(Loading == false){
-            document.body.style.overflow = "visible";
+            document.body.style.overflowY = "visible";
         }
     }else{
         Navigate.style.left = "0px";
-        document.body.style.overflow = "hidden";
+        document.body.style.overflowY = "hidden";
     }
 }
-
-
-
 
 //Fechar tela de carregamento
 function CloseScreenLoading(){
@@ -76,44 +72,43 @@ function CloseScreenLoading(){
 
 //Função pra detectar ação swipe
 function swipedetect(el, callback){
-        var touchsurface = el,
-        swipedir,
-        startX,
-        startY,
-        distX,
-        distY,
-        threshold = 100,
-        restraint = 100,
-        allowedTime = 200,
-        elapsedTime,
-        startTime,
-        handleswipe = callback
-      
-        touchsurface.addEventListener('touchstart', (e) => {
-            var touchobj = e.changedTouches[0]
-            swipedir = 'none'
-            dist = 0
-            startX = touchobj.pageX
-            startY = touchobj.pageY
-            startTime = new Date().getTime()
-        }, false)
+    var touchsurface = el,
+    swipedir,
+    startX,
+    startY,
+    distX,
+    distY,
+    threshold = 100,
+    restraint = 100,
+    allowedTime = 200,
+    elapsedTime,
+    startTime,
+    handleswipe = callback
+    touchsurface.addEventListener('touchstart', (e) => {
+        var touchobj = e.changedTouches[0]
+        swipedir = 'none'
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime()
+    }, false)
         
-        touchsurface.addEventListener('touchend', (e) => {
-            var touchobj = e.changedTouches[0]
-            distX = touchobj.pageX - startX
-            distY = touchobj.pageY - startY
-            elapsedTime = new Date().getTime() - startTime
-            if (elapsedTime <= allowedTime){
-                if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
-                    swipedir = (distX < 0)? 'left' : 'right'
-                }
-                else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){
-                    swipedir = (distY < 0)? 'up' : 'down'
-                }
-            }
-            handleswipe(swipedir)
-        }, false)
-    }
+    touchsurface.addEventListener('touchend', (e) => {
+    var touchobj = e.changedTouches[0]
+        distX = touchobj.pageX - startX
+        distY = touchobj.pageY - startY
+        elapsedTime = new Date().getTime() - startTime
+        if (elapsedTime <= allowedTime){
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
+                swipedir = (distX < 0)? 'left' : 'right'
+            }
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){
+                swipedir = (distY < 0)? 'up' : 'down'
+            }
+        }
+        handleswipe(swipedir)
+    }, false)
+}
 
 //Mostrar e esconder AppBar
 var prevScrollpos = window.pageYOffset;
